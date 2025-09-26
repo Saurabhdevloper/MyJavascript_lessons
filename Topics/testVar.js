1caefb59e0061924caa98f9187829b6cfad73f24
Saurabh Patel <152629310+Saurabhdevloper@users.noreply.github.com>
2025-09-26 21:08:27 +0000

    variables definations

---
+++ Topics/testVar.js
@@ -0,0 +1,17 @@
+
+const StdId=12123;
+var StdName="shiv";
+let city="Lucknow";
+
+State="Uttar Pradesh";
+let bal;
+
+/*
+  1. To use var to define a variable is not a good practice. Please ignore it.
+
+  2. A defined block is known as Scope.
+ */
+
+console.log(StdId);
+console.table([StdId,StdName,city,State,bal]);
+
